735563f35995f2a34482bb261f85f174f02ccfa6
Muhammad Naeem Raj <90338269+muhammadnaeemraj@users.noreply.github.com>
2022-12-07 22:44:15 +0000

    added asserts images and updated the navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
 
 function App() {
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="Container">
+            <NavbarBrand href="/">
+              <h2>Muhammad Naeem Raj</h2>
+            </NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
     </>
   );
 }
