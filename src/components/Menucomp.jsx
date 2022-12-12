a8778c803795d8004befa70bc4d6341a5b510273
MuhammadNaeem <muhammad.naeemraj@gmai.com>
2022-12-12 15:28:15 +0500

    MenuComp.jsx file updated class component to functional component

--- src/components/Menucomp.jsx
+++ src/components/Menucomp.jsx
@@ -1,40 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "bootstrap";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import "./MenuComp.css";
 import { dishes } from "../shared/Dishes";
 
-class Menu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { dishes };
-  }
-
-  render() {
-    const menu = this.state.dishes.map((dish) => {
-      return (
-        <div key={dish.id} className="col-12 mt-5">
-          <ListGroup>
-            <ListGroupItem className="list">
-              <img src={dish.image} alt={dish.name} />
-              <div className="detail">
-                <h2 className="heading">{dish.name}</h2>
-                <p className="discription">{dish.description}</p>
-              </div>
-            </ListGroupItem>
-          </ListGroup>
-        </div>
-      );
-    });
-
+const Menu = () => {
+  const [dish, setDish] = useState(dishes);
+  const menu = dish.map((d) => {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="menu">{menu}</div>
-        </div>
+      <div key={d.id} className="col-12 mt-5">
+        <ListGroup>
+          <ListGroupItem className="list">
+            <img src={d.image} alt={d.name} />
+            <div className="detail">
+              <h2 className="heading">{d.name}</h2>
+              <p className="discription">{d.description}</p>
+            </div>
+          </ListGroupItem>
+        </ListGroup>
       </div>
     );
-  }
-}
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="menu">{menu}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
