968846c2186de9f2ed0184819d235a0828be8168
MuhammadNaeem <muhammad.naeemraj@gmai.com>
2022-12-13 21:51:47 +0500

    Applying the functionality onclick on dish discription

--- src/components/Menucomp.jsx
+++ src/components/Menucomp.jsx
@@ -1,30 +1,49 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import "bootstrap";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import "./MenuComp.css";
 import { dishes } from "../shared/Dishes";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
 const Menu = () => {
   const [dish, setDish] = useState(dishes);
+  const [disc, setDisc] = useState(null);
   const menu = dish.map((d) => {
     return (
-      <div key={d.id} className="col-12 mt-5">
-        <ListGroup>
-          <ListGroupItem className="list">
-            <img src={d.image} alt={d.name} />
-            <div className="detail">
+      <div key={d.id} className="col-12 col-mt-5 m-2">
+        <Card
+          style={{
+            width: "18rem",
+          }}
+          onClick={() => setDisc(d.description)}
+        >
+          <img src={d.image} alt={d.name} />
+          <CardBody>
+            <CardImgOverlay>
+              <h5 className="heading">{d.name}</h5>
+            </CardImgOverlay>
+            <CardTitle>
               <h2 className="heading">{d.name}</h2>
-              <p className="discription">{d.description}</p>
-            </div>
-          </ListGroupItem>
-        </ListGroup>
+            </CardTitle>
+            <CardText>
+              <p className="discription">{disc}</p>
+            </CardText>
+          </CardBody>
+        </Card>
       </div>
     );
   });
   return (
     <div className="container">
       <div className="row">
-        <div className="menu">{menu}</div>
+        <div className="menu col">{menu}</div>
       </div>
     </div>
   );
